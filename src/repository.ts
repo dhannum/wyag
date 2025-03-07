55b7ba6aec766ce784814fe9b5835204b7363cb1
Daniel Hannum <daniel.hannum@gmail.com>
2025-03-06 19:51:18 -0500

    repofind

--- src/repository.ts
+++ src/repository.ts
@@ -5,7 +5,7 @@ import { writeIniFileSync } from 'write-ini-file'
 
 interface GitConfig {
     core: {
-        repositoryformatversion: number
+        repositoryformatversion: string
         filemode: boolean
         bare: boolean
     }
@@ -18,7 +18,7 @@ export class GitRepository {
 
     defaultConf: GitConfig = {
         core: {
-            repositoryformatversion: 0,
+            repositoryformatversion: '0',
             filemode: false,
             bare: false,
         },
@@ -28,15 +28,17 @@ export class GitRepository {
         this.workTree = rootPath
         this.gitDir = path.join(this.workTree, '.git')
 
-        if (!force && fs.existsSync(this.gitDir)) {
+        if (!force && !fs.existsSync(this.gitDir)) {
             throw new Error(`Not a Git repository ${path}`)
         }
 
         const configFilePath = path.join(this.gitDir, 'config')
         if (fs.existsSync(configFilePath)) {
             this.conf = readIniFileSync(configFilePath) as GitConfig
-            if (this.conf.core.repositoryformatversion !== 0) {
-                throw new Error(`Unsupported repositoryformatversion: ${8}`)
+            if (Number.parseInt(this.conf.core.repositoryformatversion) !== 0) {
+                throw new Error(
+                    `Unsupported repositoryformatversion: ${this.conf.core.repositoryformatversion}`,
+                )
             }
         } else if (!force) {
             throw new Error('Configuration file missing')
@@ -63,5 +65,14 @@ export class GitRepository {
         fs.writeFileSync(path.join(this.gitDir, 'HEAD'), 'ref: refs/heads/master\n')
 
         writeIniFileSync(path.join(this.gitDir, 'config'), this.defaultConf)
+    }
+
+    static repoFind(p: string): GitRepository | undefined {
+        if (fs.existsSync(path.join(p, '.git'))) {
+            return new GitRepository(p)
+        } else {
+            if (p === '/') return undefined
+            return this.repoFind(path.dirname(p))
+        }
     }
 }
