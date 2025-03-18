ac446daffe68034cc02e441ae1a9aad5510e362c
Daniel Hannum <daniel.hannum@creditkarma.com>
2025-03-18 11:22:01 -0400

    refactor

---
+++ test/unit/command/init.test.ts
@@ -0,0 +1,41 @@
+import { beforeEach, expect, test, vi, describe } from 'vitest'
+import { commandInit } from '../../../src/command/init.js'
+import { GitRepository } from '../../../src/repository.js'
+
+const mockRepository = {
+    create: vi.fn(),
+}
+
+describe('init command', () => {
+    beforeEach(() => {
+        vi.clearAllMocks()
+
+        vi.mock('../../../src/repository.js', () => {
+            return {
+                GitRepository: vi.fn().mockImplementation(() => mockRepository),
+            }
+        })
+    })
+
+    test('work in current dir', () => {
+        const mockCwd = '/tmp/foo'
+        vi.spyOn(process, 'cwd').mockReturnValue(mockCwd)
+
+        commandInit([''])
+
+        expect(GitRepository).toHaveBeenCalledTimes(1)
+        expect(GitRepository).toHaveBeenCalledWith(`${mockCwd}`, true)
+        expect(mockRepository.create).toHaveBeenCalledTimes(1)
+    })
+
+    test('work in provided dir', () => {
+        const mockCwd = '/tmp/foo'
+        vi.spyOn(process, 'cwd').mockReturnValue(mockCwd)
+
+        commandInit(['work'])
+
+        expect(GitRepository).toHaveBeenCalledTimes(1)
+        expect(GitRepository).toHaveBeenCalledWith(`${mockCwd}/work`, true)
+        expect(mockRepository.create).toHaveBeenCalledTimes(1)
+    })
+})
